da64b4b93821aef52673b475d91713b2a417697c
Eman Razzaq <75653812+EmanRazzaq@users.noreply.github.com>
2024-10-15 15:09:09 +0500

    Add files via upload

---
+++ Counter.java
@@ -0,0 +1,50 @@
+package exam;
+public class Counter {
+
+private int count = 0;
+
+public synchronized void increment() {
+
+count++;
+
+}
+
+public int getCount() {
+
+return count;
+
+}
+
+public static void main(String[] args) {
+
+Counter counter = new Counter();
+
+Thread thread1 = new Thread(() -> {
+
+for (int i = 0; i < 10000; i++) {
+
+counter.increment();
+
+}
+
+});
+
+Thread thread2 = new Thread(() -> {
+
+for (int i = 0; i < 10000; i++) {
+
+counter.increment();
+
+}
+
+});
+
+thread1.start();
+
+thread2.start();
+
+System.out.println("Final Count: " + counter.getCount());
+
+}
+
+}
